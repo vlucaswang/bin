dc3b1b9b3c77fe3861a31552c1c04c56ffb5d6e7
Xiaochuan Wang <nkwdwxc@gmail.com>
2015-01-13 12:00:56 +0800

    Remove sth

--- talend/download_nbuilds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm /var/samba/nightly/lastswtbot*
-cd /var/samba/nightly/
-#&& find /var/samba/nightly/ -name "*.zip" -mtime +4 -exec rm {} \;
-wget --user=build-admin --password=remy_build http://build.talend.com/builds/lastnb.txt
-#wget --input-file=lastnb.txt
-
-cat lastnb.txt |xargs wget --user=build-admin --password=remy_build -
-rm lastnb.txt
