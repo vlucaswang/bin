99b3c387350c511b0ee7b44e9585d402d33d5657
Xiaochuan Wang <nkwdwxc@gmail.com>
2015-04-29 20:32:33 +0800

    clean info

--- testing/deploy.sh
+++ testing/deploy.sh
@@ -3,6 +3,10 @@
 if [ ! -f /root/.ssh/id_rsa ]; then
 	ssh-keygen -q -t rsa -f /root/.ssh/id_rsa -N ""
 fi
+cat >/root/.ssh/config <<EOF
+Host *
+    StrictHostKeyChecking no
+EOF
 ssh-copy-id root@$1
 scp Linux_check.sh root@$1:/tmp/
 ssh root@$1 'chmod +x /tmp/Linux_check.sh;bash /tmp/Linux_check.sh'
