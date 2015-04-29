c962c8fffaa57b486ad2a751d90cdcf5d056cd0e
Xiaochuan Wang <nkwdwxc@gmail.com>
2015-04-29 10:49:14 +0800

    clean info

--- scrcu/passwdchange.sh
+++ scrcu/passwdchange.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+PASSWD=
+
 while read line; do
   ssh -n $line 'echo sierra | passwd --stdin root'
   ssh $line 2>&1 >result.txt << EOF
     for i in $(awk -F: '{if ($3 >= 500) { print $1 } }' /etc/passwd)
     do 
-       echo sysSCRCU | passwd --stdin $i
+       echo $PASSWD | passwd --stdin $i
     done
   EOF
 done < ip.txt
