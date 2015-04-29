27dcce53a08e80c304e222b4863e54d576a7a244
Xiaochuan Wang <nkwdwxc@gmail.com>
2015-04-29 15:21:16 +0800

    clean info

--- testing/deploy.sh
+++ testing/deploy.sh
@@ -2,7 +2,7 @@
 
 ssh-copy-id root@$1
 scp Linux_check.sh root@$1:/tmp/
-ssh root@$1 'chmod +x Linux_check.sh;bash /tmp/Linux_check.sh'
+ssh root@$1 'chmod +x /tmp/Linux_check.sh;bash /tmp/Linux_check.sh'
 scp -r root@$1:/tmp/sysssc/* .
 ssh root@$1 "sed -i '\$d' .ssh/authorized_keys;rm -rf /tmp/sysssc;rm -f /tmp/Linux_check.sh"
 
