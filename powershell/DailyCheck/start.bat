4f5044e7de77b95ac1db08b7113b914e6c0ebf82
Xiaochuan Wang <nkwdwxc@gmail.com>
2016-06-24 16:52:30 +0930

    Correct spelling errors.

---
+++ powershell/DailyCheck/start.bat
@@ -0,0 +1,9 @@
+powershell.exe Set-ExecutionPolicy Unrestricted
+
+md -Force c:\supports\scripts
+
+#powershell.exe Invoke-WebRequest -OutFile c:\supports\scripts\Task_Scheduler_Setup.ps1 http://52.62.250.69/scripts/Task_Scheduler_Setup.ps1
+
+#powershell.exe Invoke-WebRequest -OutFile c:\supports\scripts\Daily_Check_ShortVer.ps1 http://52.62.250.69/scripts/Daily_Check_ShortVer.ps1
+
+#powershell.exe -File c:\supports\scripts\Task_Scheduler_Setup.ps1
